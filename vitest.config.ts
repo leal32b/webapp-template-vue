d928d3c3f84684f10128d4ebc4bbe9284acdf37a
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2023-11-03 11:11:40 -0300

    chore: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    root: '.',
+    globals: true,
+    include: ['test/**/*.{integration,unit}-test.ts'],
+    exclude: ['test/**/_doubles/**'],
+    threads: false,
+    watch: false,
+    silent: true,
+    logHeapUsage: false,
+    passWithNoTests: true,
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: [],
+      provider: 'istanbul',
+      reporter: ['text-summary', 'html', 'lcov'],
+      statements: 100
+    }
+  }
+})
