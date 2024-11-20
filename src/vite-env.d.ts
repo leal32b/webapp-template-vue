e5916342be82e6e75a1dd2ba81ae06becdf5e4f5
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2024-11-20 16:57:22 -0300

    fix: $t function

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,5 +5,6 @@ import { type MessageType } from '@/common/2.presentation/i18n/locales/en-US'
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $m: MessageType
+    $t: (key: string) => string
   }
 }
