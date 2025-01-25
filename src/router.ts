9742ca0ffce2f838f9de006444b537e4ae70fbc0
Fernando Oliveira <fernando.oliveira@gupy.com.br>
2025-01-25 18:59:51 -0300

    fix: fix createWebHistory

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,7 @@ import { commonRoutes } from '@/common/2.presentation/routes/common.routes'
 import { identityRoutes } from '@/modules/identity/2.presentation/routes/identity.routes'
 
 export default createRouter({
-  history: createWebHistory(process.env.VITE_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     ...commonRoutes,
     ...identityRoutes
